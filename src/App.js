c1d018de8c09f280b798e4ed31c9cefe8bcee9ba
Serhii Herych <mac@MacBook-Air-mac.local>
2023-07-04 19:38:22 +0300

    caunter- app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './index.css';
 
+import React, {useState} from 'react';
+
 function App() {
+  const [caount, setCount] = useState(0);
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{caount}</h1>
+        <button onClick={() => setCount(caount - 1)} className="minus">- Минус</button>
+        <button onClick={() => setCount(caount + 1)}  className="plus">Плюс +</button>
       </div>
     </div>
   );
